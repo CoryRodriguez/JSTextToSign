7bf10bdf7928114df9d623b9120eebd77b108de9
CoryRodriguez <mrcoryrodriguez@gmail.com>
2018-04-03 21:11:36 -0500

    tested some JS possibilities for functionality

--- src/js/app3.js
+++ src/js/app3.js
@@ -2,13 +2,17 @@ const lettersInput = document.getElementById('lettersInput');
 const signOutput = document.getElementById('signOutput');
 const inputForm = document.getElementById('inputForm');
 
+// Prevent Default
 inputForm.addEventListener('submit', e => {
   e.preventDefault();
 });
 
+
 lettersInput.addEventListener('input', (e) => {
-  let letter = e.data;
-  console.log(letter);
+  let letter = e.target.value.split('');
+
+  //myArray.push(lettersInput.value)
+  // console.log(letter);
 
   if(letter === null){
     console.log(signOutput.removeChild(signOutput.childNodes[1]));
@@ -18,6 +22,31 @@ lettersInput.addEventListener('input', (e) => {
     <img src="./img/sign${letter}.png" class="img-fluid">
   `;
   }
+
+  //console.log(letter);
+  // for(i = 0; i < letter.length; i++){
+  //   console.log(letter[i]);
+  // }
 });
 
+function pushData(){
+  lettersInput.addEventListener('keyup',(e)=>{
+    let test = e.key;
+    console.log(e.key);
+    if (test === null) {
+      console.log(signOutput.removeChild(signOutput.childNodes[1]));
+      //signOutput.innerHTML = ``;
+    } else {
+      signOutput.innerHTML += `
+    <img src="./img/sign${test}.png" class="img-fluid">
+  `;
+    }
+  });
+}
+pushData();
+
 console.log(lettersInput.value);
+
+
+
+// console.log(lettersInput.value);
