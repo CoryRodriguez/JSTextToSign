b4984e987711e507ca0ebfe1de3bfcb5d984b3f6
CoryRodriguez <mrcoryrodriguez@gmail.com>
2018-04-04 21:03:00 -0500

    edits to JS

---
+++ src/js/app4.js
@@ -0,0 +1,53 @@
+const lettersInput = document.getElementById('lettersInput');
+const signOutput = document.getElementById('signOutput');
+const inputForm = document.getElementById('inputForm');
+
+// Prevent Default
+inputForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+
+lettersInput.addEventListener('input', (e) => {
+  let letter = e.target.value.split('');
+  
+  console.log(letter);
+  //myArray.push(lettersInput.value)
+  // console.log(letter);
+
+  // if(letter === null){
+  //   console.log(signOutput.removeChild(signOutput.childNodes[1]));
+  //   //signOutput.innerHTML = ``;
+  // } else{
+  // signOutput.innerHTML += `
+  //   <img src="./img/sign${letter}.png" class="img-fluid">
+  // `;
+  // }
+
+  //console.log(letter);
+  // for(i = 0; i < letter.length; i++){
+  //   console.log(letter[i]);
+  // }
+});
+
+// function pushData(){
+//   lettersInput.addEventListener('keyup',(e)=>{
+//     let test = e.key;
+//     console.log(e.key);
+//     if (test === undefined) {
+//       console.log(signOutput.removeChild(signOutput.childNodes[1]));
+//       //signOutput.innerHTML = ``;
+//     } else {
+//       signOutput.innerHTML += `
+//     <img src="./img/sign${test}.png" class="img-fluid">
+//   `;
+//     }
+//   });
+// }
+// pushData();
+
+console.log(lettersInput.value);
+
+
+
+// console.log(lettersInput.value);
